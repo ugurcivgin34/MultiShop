78df7435160293011463b849e327eaef42fc6ca8
Uğur Okan ÇIVGİN <uocivgin@isbak.istanbul>
2024-09-23 14:39:59 +0300

    Ürün Sayfası,Alışveriş Sepeti Sayfa Tasarımları tamamlandı

--- Frontends/MultiShop.WebUI/wwwroot/online-shop-website-template/mail/contact.js
+++ Frontends/MultiShop.WebUI/wwwroot/online-shop-website-template/mail/contact.js
@@ -1,5 +1,4 @@
 $(function () {
-
     $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function ($form, event, errors) {
@@ -27,17 +26,17 @@ $(function () {
                 success: function () {
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
+                        .append("</button>");
                     $('#success > .alert-success')
-                            .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Your message has been sent. </strong>");
                     $('#success > .alert-success')
-                            .append('</div>');
+                        .append('</div>');
                     $('#contactForm').trigger("reset");
                 },
                 error: function () {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
+                        .append("</button>");
                     $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
                     $('#success > .alert-danger').append('</div>');
                     $('#contactForm').trigger("reset");
