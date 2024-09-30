a74e89fd423ce7c00f5c561af2f0893657764ba1
Uğur Okan Çivgin <u.civgin@gmail.com>
2024-10-01 00:33:18 +0300

    Yorum İşlemlerl tamamlandı

---
+++ Services/Comment/MultiShop.Comment/Context/CommentContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Comment.Entities;
+
+namespace MultiShop.Comment.Context
+{
+    public class CommentContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;initial Catalog=MultiShopCommentDb;integrated Security=true;TrustServerCertificate=true");
+        }
+        public DbSet<UserComment> UserComments { get; set; }
+    }
+}
---
+++ Services/Comment/MultiShop.Comment/Program.cs
@@ -0,0 +1,25 @@
+using MultiShop.Comment.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<CommentContext>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
