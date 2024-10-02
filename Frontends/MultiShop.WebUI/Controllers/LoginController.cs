fb63cc9a3c89d46133036baddb7b69e136780108
Uğur Okan Çivgin <u.civgin@gmail.com>
2024-10-02 22:44:31 +0300

    Discovery Metotları

--- Frontends/MultiShop.WebUI/Controllers/LoginController.cs
+++ Frontends/MultiShop.WebUI/Controllers/LoginController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using MultiShop.DtoLayer.IdentityDtos.LoginDtos;
 using MultiShop.WebUI.Models;
 using MultiShop.WebUI.Services;
+using MultiShop.WebUI.Services.Interfaces;
 using Newtonsoft.Json;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -16,11 +17,12 @@ namespace MultiShop.WebUI.Controllers
     {
         private readonly IHttpClientFactory _httpClientFactory;
         private readonly ILoginService _loginService;
-
-        public LoginController(IHttpClientFactory httpClientFactory, ILoginService loginService)
+        private readonly IIdentityService _identityService;
+        public LoginController(IHttpClientFactory httpClientFactory, ILoginService loginService, IIdentityService identityService)
         {
             _httpClientFactory = httpClientFactory;
             _loginService = loginService;
+            _identityService = identityService;
         }
 
         public IActionResult Index()
@@ -64,6 +66,21 @@ namespace MultiShop.WebUI.Controllers
                 }
             }
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignInDto signInDto)
+        {
+            signInDto.Username = "ali01";
+            signInDto.Password = "1111aA*";
+            await _identityService.SignIn(signInDto);
+            return RedirectToAction("Index", "Test");
         }
     }
 }
---
+++ Frontends/MultiShop.WebUI/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,12 @@
+﻿using MultiShop.DtoLayer.IdentityDtos.LoginDtos;
+
+namespace MultiShop.WebUI.Services.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<bool> SignIn(SignInDto signInDto);
+    }
+
+ 
+
+}
