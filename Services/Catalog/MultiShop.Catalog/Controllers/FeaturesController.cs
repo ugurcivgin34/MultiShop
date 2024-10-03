e10ab514b32af6c0d7d9b8b2b18b08873f8f96da
Uğur Okan ÇIVGİN <uocivgin@isbak.istanbul>
2024-10-03 09:52:14 +0300

    .

--- Services/Catalog/MultiShop.Catalog/Controllers/FeaturesController.cs
+++ Services/Catalog/MultiShop.Catalog/Controllers/FeaturesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using MultiShop.Catalog.Dtos.FeatureDtos;
 using MultiShop.Catalog.Services.FeatureServices;
 
